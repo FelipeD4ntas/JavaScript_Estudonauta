297de8f838f3db39f78a6831f913caa2778d8ce6
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-01-17 18:16:13 -0300

    att
    Adicionado soma, e média.
    Tudo ok

--- Exercicios/ex020/script.js
+++ Exercicios/ex020/script.js
@@ -3,10 +3,14 @@ let btn_saida = document.getElementsByTagName('button')[1];
 let total = document.querySelector('#total');
 let maior = document.querySelector('#maior');
 let menor = document.querySelector('#menor');
+let soma = document.querySelector('#soma');
+let media = document.querySelector('#media');
 let n_maior = null;
 let n_menor = null;
 let numero = null;
 let cont = 0;
+let soma_total = 0;
+let media_total = null;
 let colecao_numero = [];
 
 btn_entrada.addEventListener('click', entrada);
@@ -17,7 +21,8 @@ btn_saida.addEventListener('click', finalizar);
 
 function entrada() {
     let saida = document.querySelector('#saida');
-    numero = Number(document.querySelector('#entrada').value)
+    numero = Number(document.querySelector('#entrada').value);
+    soma_total += numero;
     colecao_numero.push(numero);
     saida.innerHTML += `<li>Valor ${numero} cadastrado.</li>`;
 
@@ -29,7 +34,7 @@ function entrada() {
     } else if (colecao_numero[cont] < n_menor) {
         n_menor = colecao_numero[cont];
     }
-
+    media_total = soma_total / colecao_numero.length;
     cont++
 }
 
@@ -37,4 +42,6 @@ function finalizar() {
     total.innerHTML = `Ao todo, temos ${colecao_numero.length} números cadastrados.`;
     maior.innerHTML = `O maior número cadastrado foi ${n_maior}.`;
     menor.innerHTML = `O menor número cadastrado foi ${n_menor}.`;
+    soma.innerHTML = `Somando todos os valores, temos ${soma_total}.`;
+    media.innerHTML = `A média dos valores cadastrados é ${media_total.toFixed(2)}.`
 }
